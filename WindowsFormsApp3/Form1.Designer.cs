9b5530d3a435ac2db8f40f2476e56e2cdc83eb12
sober607 <sober607@gmail.com>
2020-03-14 16:39:13 +0200

    Windows forms xml

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+            
+        }
+        
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonChooseColor = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2chooseTextColor = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.buttonFontParams = new System.Windows.Forms.Button();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.saveButton1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.ForeColor = System.Drawing.Color.RosyBrown;
+            this.label1.Location = new System.Drawing.Point(83, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Some text of XML saving format";
+            // 
+            // buttonChooseColor
+            // 
+            this.buttonChooseColor.Location = new System.Drawing.Point(25, 25);
+            this.buttonChooseColor.Name = "buttonChooseColor";
+            this.buttonChooseColor.Size = new System.Drawing.Size(131, 23);
+            this.buttonChooseColor.TabIndex = 0;
+            this.buttonChooseColor.Text = "Background color";
+            this.buttonChooseColor.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(109, 23);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(211, 100);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.buttonChooseColor);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(203, 74);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Фон";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2chooseTextColor);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(203, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Текст";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2chooseTextColor
+            // 
+            this.button2chooseTextColor.Location = new System.Drawing.Point(28, 25);
+            this.button2chooseTextColor.Name = "button2chooseTextColor";
+            this.button2chooseTextColor.Size = new System.Drawing.Size(142, 23);
+            this.button2chooseTextColor.TabIndex = 0;
+            this.button2chooseTextColor.Text = "Text color";
+            this.button2chooseTextColor.UseVisualStyleBackColor = true;
+            this.button2chooseTextColor.Click += new System.EventHandler(this.button2chooseTextColor_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.buttonFontParams);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(203, 74);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Шрифт";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // buttonFontParams
+            // 
+            this.buttonFontParams.Location = new System.Drawing.Point(55, 26);
+            this.buttonFontParams.Name = "buttonFontParams";
+            this.buttonFontParams.Size = new System.Drawing.Size(75, 23);
+            this.buttonFontParams.TabIndex = 0;
+            this.buttonFontParams.Text = "Font params";
+            this.buttonFontParams.UseVisualStyleBackColor = true;
+            this.buttonFontParams.Click += new System.EventHandler(this.buttonFontParams_Click);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.saveButton1);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(203, 74);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Сохранение";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // saveButton1
+            // 
+            this.saveButton1.Location = new System.Drawing.Point(35, 25);
+            this.saveButton1.Name = "saveButton1";
+            this.saveButton1.Size = new System.Drawing.Size(75, 23);
+            this.saveButton1.TabIndex = 0;
+            this.saveButton1.Text = "Save settings";
+            this.saveButton1.UseVisualStyleBackColor = true;
+            this.saveButton1.Click += new System.EventHandler(this.saveButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 291);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.OnClickChooseColor);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonChooseColor;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2chooseTextColor;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button buttonFontParams;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button saveButton1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Reflection;
+using System.Configuration;
+using System.Collections.Specialized;
+using Microsoft.Win32;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private ColorDialog chooseColorDialog = new ColorDialog();
+        private ColorDialog chooseColorDialog1 = new ColorDialog();
+        private FontDialog chooseFontStyleDialog = new FontDialog();
+        public Form1()
+        {
+            InitializeComponent();
+            buttonChooseColor.Click += new EventHandler(OnClickChooseColor);
+            try
+            {
+                if (ReadSettings() == false)
+                {
+                    MessageBox.Show("В файле конфигурации нет информации...");
+                }
+                
+
+                this.StartPosition = FormStartPosition.CenterScreen;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            
+        }
+
+        void OnClickChooseColor(object Sender, EventArgs e)
+        {
+            if (chooseColorDialog.ShowDialog() == DialogResult.OK)
+                this.BackColor = chooseColorDialog.Color;
+        }
+
+
+        bool ReadSettings()
+        {
+            // Загрузка настроек по парам [ключ]-[значение].
+            NameValueCollection allAppSettings = ConfigurationManager.AppSettings;
+            if (allAppSettings.Count < 1) { return (false); }
+
+            // Восстановление состояния:
+            //1. Цвет фона.
+            int red = Convert.ToInt32(allAppSettings["BackGroundColor.R"]);
+            int green = Convert.ToInt32(allAppSettings["BackGroundColor.G"]);
+            int blue = Convert.ToInt32(allAppSettings["BackGroundColor.B"]);
+
+            //2. Цвет текста
+            int redText = Convert.ToInt32(allAppSettings["TextColor.R"]);
+            int greedText = Convert.ToInt32(allAppSettings["TextColor.G"]);
+            int blueText = Convert.ToInt32(allAppSettings["TextColor.B"]);
+
+            string fontName = Convert.ToString(allAppSettings["TextStyleFont.Name"]);
+            float textSize = Convert.ToSingle(allAppSettings["TextStyleFont.Size"]);
+            FontStyle fontstyle = GetStyleType();
+            // определение стиля
+            FontStyle GetStyleType ()
+            {
+                if (allAppSettings["TextStyleFont.Style"] == "Bold")
+                {
+                    return FontStyle.Bold;
+                }
+                else if (allAppSettings["TextStyleFont.Style"] == "Italic")
+                {
+                    return FontStyle.Italic;
+                }
+                else if (allAppSettings["TextStyleFont.Style"] == "Regular")
+                {
+                    return FontStyle.Regular;
+                }
+                else if (allAppSettings["TextStyleFont.Style"] == "Strikeout")
+                {
+                    return FontStyle.Strikeout;
+                }
+                else
+                {
+                    return FontStyle.Regular;
+                }
+            
+            }
+                
+
+            // установка
+            this.BackColor = Color.FromArgb(red, green, blue);
+            label1.ForeColor= Color.FromArgb(redText, greedText, blueText);
+            label1.Font= new Font(fontName, textSize, fontstyle);
+
+            
+
+            //4. Состояние окна.
+            string winState = allAppSettings["Window.State"];
+            
+            this.WindowState = (FormWindowState)FormWindowState.Parse(WindowState.GetType(), winState);
+            return (true);
+        }
+        void SaveSettings()
+        {
+            
+            // Сохранение происходит при помощи работы с XML.
+            XmlDocument doc = loadConfigDocument();
+
+            // Открываем узел appSettings, в котором содержится перечень настроек.
+            XmlNode node = doc.SelectSingleNode("//appSettings");
+
+            // Массив ключей (создан для упрощения обращения к файлу конфигурации).
+            string[] keys = new string[] {"BackGroundColor.R",
+                                          "BackGroundColor.G",
+                                          "BackGroundColor.B",
+                                          "TextColor.R",
+                                          "TextColor.G",
+                                          "TextColor.B",
+                                          "TextStyleFont.Name",
+                                          "TextStyleFont.Size",
+                                          "TextStyleFont.Style",
+
+                                          "Window.State"};
+
+            // Массив значений (создан для упрощения обращения к файлу конфигурации).
+            string[] values = new string[] { BackColor.R.ToString(),
+                                             BackColor.G.ToString(),
+                                             BackColor.B.ToString(),
+                                             label1.ForeColor.R.ToString(),
+                                             label1.ForeColor.G.ToString(),
+                                             label1.ForeColor.B.ToString(),
+                                             label1.Font.Name.ToString(),
+                                             label1.Font.Size.ToString(),
+                                             label1.Font.Style.ToString(),
+
+                                             WindowState.ToString() };
+
+            // Цикл модификации файла конфигурации.
+            for (int i = 0; i < keys.Length; i++)
+            {
+                // Обращаемся к конкретной строке по ключу.
+                XmlElement element = node.SelectSingleNode(string.Format("//add[@key='{0}']", keys[i])) as XmlElement;
+
+                // Если строка с таким ключем существует - записываем значение.
+                if (element != null) { element.SetAttribute("value", values[i]); }
+                else
+                {
+                    // Иначе: создаем строку и формируем в ней пару [ключ]-[значение].
+                    element = doc.CreateElement("add");
+                    element.SetAttribute("key", keys[i]);
+                    element.SetAttribute("value", values[i]);
+                    node.AppendChild(element);
+                }
+            }
+
+            // Сохраняем результат модификации.
+            doc.Save(Assembly.GetExecutingAssembly().Location + ".config");
+            
+        }
+
+        private static XmlDocument loadConfigDocument()
+        {
+            XmlDocument doc = null;
+            try
+            {
+                doc = new XmlDocument();
+                doc.Load(Assembly.GetExecutingAssembly().Location + ".config");
+                return doc;
+            }
+            catch (System.IO.FileNotFoundException e)
+            {
+                throw new Exception("No configuration file found.", e);
+            }
+        }
+
+        private void button2chooseTextColor_Click(object sender, EventArgs e)
+        {
+            if (chooseColorDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // установка цвета формы
+            label1.ForeColor = chooseColorDialog1.Color;
+        }
+
+        private void buttonFontParams_Click(object sender, EventArgs e)
+        {
+           if (chooseFontStyleDialog.ShowDialog() == DialogResult.Cancel)
+           return;
+            // установка шрифта
+            label1.Font = chooseFontStyleDialog.Font;
+
+            
+        }
+
+        private void saveButton1_Click(object sender, EventArgs e)
+        {
+            SaveSettings();
+        }
+    }
+}
